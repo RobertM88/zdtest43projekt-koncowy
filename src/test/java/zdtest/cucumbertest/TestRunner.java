a2d2bec6d4ef9811fb100467ea1a052d55f5c13b
RobertM88 <r.markucinski@gmail.com>
2020-12-17 18:18:02 +0100

    created bdd and selenium tests

---
+++ src/test/java/zdtest/cucumbertest/TestRunner.java
@@ -0,0 +1,10 @@
+package zdtest.cucumbertest;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/zdtest.cucumbertest"
+)
+public class TestRunner {
+}
